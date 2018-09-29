76ab01cdb5ac930e7cb2f0a62bf827be844e1f65
Rails Test App <railsTestApp@gmail.com>
2018-09-29 16:09:36 +0530

    alpha-blog-tut-CRUD-operations

---
+++ test/controllers/page_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class PageControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get page_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get page_about_url
+    assert_response :success
+  end
+
+end
